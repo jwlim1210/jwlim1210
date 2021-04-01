9b47c3275c5b2efe12353de6a6c7ce9d42fbee7c
jwlim1210 <jwlim1210@naver.com>
2021-04-01 11:08:47 +0900

    sum 중간 값 확인 소스 추가

--- main.java
+++ main.java
@@ -2,7 +2,9 @@ pulic class main {
 	pulic static void main (String[] args) {
 	int sum =0;
 	for(int i=1; i<=100; i++) {
-	sum = sum + i;	 
+	sum = sum + i;
+	// sum의 중간 값 확인
+	System.out.println(sum);
 	}
 	System.out.println(sum);
        }
